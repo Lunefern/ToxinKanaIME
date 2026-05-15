72739b4dc9a66baf02e436d51be35b8ccf5b86f5
Moonshine <63332602+Lunefern@users.noreply.github.com>
2026-05-15 17:22:07 +0800

    Delete build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug directory

--- build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ToxinKanaIME_FILE E:/DOC2/QtProject/ToxinKanaIME/ToxinKanaIME/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/ToxinKanaIME.exe)
-set(__QT_DEPLOY_TARGET_ToxinKanaIME_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ToxinKanaIME_RUNTIME_DLLS E:/A/QT/6.11.0/mingw_64/bin/Qt6Widgets.dll;E:/A/QT/6.11.0/mingw_64/bin/Qt6Gui.dll;E:/A/QT/6.11.0/mingw_64/bin/Qt6Core.dll)
